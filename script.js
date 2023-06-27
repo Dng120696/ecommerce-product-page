1852901b6f97908551c00594410044c79931f617
Dng16 <dongyang0016@gmail.com>
2023-06-27 23:30:31 +0800

    initial commit

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ const imgSlide = document.querySelectorAll(".slide-img");
 const previousBtn = document.querySelector(".previous--btn");
 const nextBtn = document.querySelector(".next--btn");
 let currSlide = 0;
+let currentIndexproduct = 0;
 const maxSlide = imgSlide.length - 1;
 const goToSlide = (curr) => {
   imgSlide.forEach(
@@ -76,7 +77,6 @@ const mainContainer = document.querySelector(".main-container");
 
 const cartContainer = document.querySelector(".cart-container");
 cartBox.addEventListener("click", () => {
-  console.log("click");
   cartContainer.classList.toggle("show-cart");
 });
 
@@ -84,43 +84,47 @@ let purchaseval = [];
 // BTN CART
 const btnCart = document.querySelector(".btn-add-to-cart");
 const basketBox = document.querySelector(".basket-box");
+const cartCount = document.querySelector(".cart-count");
+let total = parseInt(cartCount.textContent);
 
 const addPurchase = (purchase) => {
   basketBox.innerHTML = "";
-  purchase.forEach((item, i) => {
-    if (quantity >= 1) {
-      const slideIndex = i % (maxSlide + 1); // Calculate the slide index using modulo operator
-      if (slideIndex <= maxSlide) {
-        document.querySelector(".basket-empty")?.remove();
-        const html = `<div class = 'items-purchase flex flex-col'>
-          <div class = 'item flex'>
-            <img src = './images/image-product-${
-              slideIndex + 1
-            }.jpg' class = 'img-item'>
-            <p>
-            <span class = 'item-name'>
-            Fall Limited Edition Sneakers
-            </span>
-            <span class = 'item-price'>
-            $125.00 x ${item}
-            </span>
-            <span class = 'font-bold'>
-              $${(125 * item).toFixed(2)}
-             </span>
-            </p>
-            <img src = './images/icon-delete.svg' class='delete-box' data-del = '${i}'>
-          </div>
-          <button class = 'btn btn-checkout'>Checkout</button>
-        </div>`;
-        basketBox.insertAdjacentHTML("afterbegin", html);
+  if(purchase.length === 0){
+    basketBox.insertAdjacentHTML('afterbegin',`<p class = 'basket-empty'> Your cart is empty.
+    </p>`);
+  }else{
+
+    purchase.forEach((item, i) => {
+      if (item >= 1) {
+  
+          const html = `<div class = 'items-purchase flex flex-col'>
+            <div class = 'item flex'>
+              <img src = './images/image-product-${currentIndexproduct + 1}.jpg' class = 'img-item'>
+              <p>
+              <span class = 'item-name'>
+              Fall Limited Edition Sneakers
+              </span>
+              <span class = 'item-price'>
+              $125.00 x ${item}
+              </span>
+              <span class = 'font-bold'>
+                $${(125 * item).toFixed(2)}
+               </span>
+              </p>
+              <img src = './images/icon-delete.svg' class='delete-box' data-del = '${i}'>
+            </div>
+            <button class = 'btn btn-checkout'>Checkout</button>
+          </div>`;
+          basketBox.insertAdjacentHTML("afterbegin", html);
+        
+      } else {
+        return;
       }
-    } else {
-      return;
-    }
-  });
+    
+      
+    });
+  }
 };
-const cartCount = document.querySelector(".cart-count");
-let total = parseInt(cartCount.textContent);
 btnCart.addEventListener("click", () => {
   if (!quantity) return;
   purchaseval.push(quantity);
@@ -139,10 +143,7 @@ basketBox.addEventListener("click", function (e) {
     addPurchase(purchaseval);
   }
 
-  if (purchaseval.length === 0) {
-    const newElement = document.createElement("p");
-    newElement.textContent = "Your cart is empty.";
-    this.appendChild(newElement);
+  if (!purchaseval.length) {
     cartCount.innerHTML = "";
   }
 });
@@ -181,21 +182,26 @@ imageProducts.addEventListener("click", function (e) {
   const el = e.target;
   // console.log(el);
   if (el.classList.contains("img-product")) {
-    const { product } = el.dataset;
+    currentIndexproduct = +el.dataset.product;
     imageSection.classList.add("active");
     overlay.classList.add("show-overlay");
-    goToSlide(product);
-  }
+    goToSlide(currentIndexproduct);
+      thumbnails(currentIndexproduct);
+      product(currentIndexproduct);
+    }
+  
 });
 
 imageThumbnails.addEventListener("click", function (e) {
   const el = e.target;
   if (el.classList.contains("img-thumbnails")) {
-    const { index } = el.dataset;
+    currentIndexproduct = +el.dataset.index;
     imgThumbnails.forEach((thumb) =>
       thumb.classList.remove("img-thumbnails--active")
     );
     el.classList.add("img-thumbnails--active");
-    product(index);
+    goToSlide(currentIndexproduct);
+    thumbnails(currentIndexproduct);
+    product(currentIndexproduct);
   }
 });
